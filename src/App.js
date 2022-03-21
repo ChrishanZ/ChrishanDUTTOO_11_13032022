ff242c7448285d4578738be6af845adacede01b8
Chrishan.Duttoo <chrisduttoo@gmail.com>
2022-03-21 22:55:32 +0100

    front|| fix lint bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./styles/App.scss";
 //Import the pages
 import Propos from "./views/propos";
 import Error from "./views/error";
-import Composants from "./views/composants";
 import Homepage from "./views/homepage";
 import Description from "./views/description";
 
@@ -20,11 +19,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="*" element={<Error />} />
           <Route path="/" element={<Homepage />} />
           <Route path="propos" element={<Propos />} />
           <Route path="model/:id" element={<Description />} />
-          <Route path="composants" element={<Composants />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </Router>
